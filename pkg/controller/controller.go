7210162b691ad398da75e4d1eeb84545e47afdda
Codacy Archive <codacy-archive@local>
2026-03-04 01:23:50 +0000

    snapshot 2026-03-04T01:23:50.458395+00:00

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -568,6 +568,13 @@ func (c *Controller) Rotate(content []byte) ([]byte, error) {
 
 	switch s := object.(type) {
 	case *ssv1alpha1.SealedSecret:
+		// Verify metainformation is well set up in Template ObjectMeta and ObjectMeta to avoid unconsistences with the scope during the rotate.
+		// This is going to keep the original scope.
+		if !reflect.DeepEqual(s.ObjectMeta, s.Spec.Template.ObjectMeta) {
+			s.ObjectMeta.DeepCopyInto(&s.Spec.Template.ObjectMeta)
+			slog.Warn("Sealed Secret metadata doesn't match. Please align your Sealed Secret metadata")
+		}
+
 		secret, err := c.attemptUnseal(s)
 		if err != nil {
 			return nil, fmt.Errorf("error decrypting secret. %v", err)
